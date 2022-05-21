7acaef47e5023cdf65a1dd229dbb120f7d1dec1b
Alfredo Figueroa <alfredojfigueroa@gmail.com>
2022-05-20 21:23:18 -0400

    started working on the fucntions in the server.js file to start the app.

---
+++ server.js
@@ -0,0 +1,62 @@
+const mysql = require('mysql2');
+const inquirer = requier('inquirer');
+const consoleTable = require('console.table');
+
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      database: 'employees_db'
+    },
+    console.log(`Connected to the employees_db database.`)
+  );
+
+  db.connect(function(err) {
+      if (err) throw err;
+      startApp();
+  });
+
+function startApp() {
+    inquirer.prompt ({
+        name: 'choice',
+        type: 'list',
+        message: 'Welcome to Employee Tracker! What would you like to do?',
+        choices: [
+            'View all employees',
+            'View all departments',
+            'View all roles',
+            'Add an employee',
+            'Add department',
+            'Add a role',
+            'Exit'
+        ]
+    }).then(function (answer) {
+        switch (answer.choice) {
+            case 'View all employees':
+                viewEmployees();
+                break;
+            case 'View all departments':
+                viewDepartments();
+                break;
+            case 'View all roles':
+                viewRoles();
+                break;
+            case 'Add an employee':
+                addEmployee();
+                break;
+            case 'Add department':
+                addDepartment();
+                break;
+            case 'Add a role':
+                addRole();
+                break;
+            case 'Exit':
+                endApp();
+                break;
+            default:
+                break;
+        }
+    })
+}
+
